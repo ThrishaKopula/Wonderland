50e4e82bc2ecf3dabb0da44d79c825b47c257095
Junle Yuan <leo905991717@gmail.com>
2023-04-09 22:49:26 -0700

    map

--- Scenes/leoStuff/map.gd
+++ Scenes/leoStuff/map.gd
@@ -5,18 +5,40 @@ extends CanvasLayer
 # var a = 2
 # var b = "text"
 
+var hid = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if Input.is_action_just_pressed("map") and !hid:
+		hide()
+		hid = true
+	elif Input.is_action_just_pressed("map") and hid:
+		show()
+		hid = false
+	pass
 
 
 func _on_Button_mouse_entered():
 	print("lol")
+	pass # Replace with function body.
+
+func _on_Button_mouse_exited():
+	print("lol out")
+	pass # Replace with function body.
+
+func _on_Button_hide():
+	print("lol out")
+	pass # Replace with function body.
+
+
+func _on_Button2_mouse_entered():
+	print("lol2")
 	pass # Replace with function body.
 
--- code/player.gd
+++ code/player.gd
@@ -1,18 +1,13 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
 var motion = Vector2()
 
 export var speed = 100
 
-# Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	motion = move_and_slide(motion)
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	if Input.is_action_pressed("move_right"):
