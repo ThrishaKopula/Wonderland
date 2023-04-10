4a0c5d650e4107b4cc254e6218171b1a173ab79b
Junle Yuan <leo905991717@gmail.com>
2023-04-09 21:54:52 -0700

    player

--- code/player.gd
+++ code/player.gd
@@ -3,14 +3,22 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"
+var motion = Vector2()
 
+export var speed = 100
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _physics_process(delta):
+	motion = move_and_slide(motion)
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if Input.is_action_pressed("move_right"):
+		motion.x = speed
+	elif Input.is_action_pressed("move_left"):
+		motion.x = -speed
+	else:
+		motion.x = 0
+	pass
