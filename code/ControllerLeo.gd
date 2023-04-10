efcd5d594e9d48fed875b17724543412978cd62a
Junle Yuan <leo905991717@gmail.com>
2023-04-09 21:28:29 -0700

    now?

---
+++ code/ControllerLeo.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+#var map = preload("res://Scenes/leoStuff/map.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	#var instance = ControllerLeo.map.instance()
+	#add_child(instance)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
