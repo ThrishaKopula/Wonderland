4a53b31054c04b8d2b5657f7032d686fcc6eab33
Thrisha Kopula <thrishak1@gmail.com>
2023-04-11 23:23:57 -0700

    started adding prologue

--- Scenes/inventory.gd
+++ Scenes/inventory.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var dialog = Dialogic.start("movingIn")
+	var dialog = Dialogic.start("movingInPart2")
 	add_child(dialog)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
