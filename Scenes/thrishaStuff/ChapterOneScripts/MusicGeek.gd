e1168742fc8771bf8a21f5d6324c71bdcc00b770
Thrisha Kopula <thrishak1@gmail.com>
2023-04-27 18:42:16 -0700

    NPC quest interaction logic

--- Scenes/thrishaStuff/ChapterOneScripts/MusicGeek.gd
+++ Scenes/thrishaStuff/ChapterOneScripts/MusicGeek.gd
@@ -2,41 +2,35 @@ extends Area2D
 
 var active = false
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	connect("body_entered", self, "_on_Bed_body_entered")
-#	connect("body_exited", self, "_on_Bed_body_exited")
-	
 func _process(_delta):
 	$QuestionMark.visible = active
 
 func _input(event):
 	if get_node_or_null('DialogNode') == null:
 		if event.is_action_pressed("ui_accept") and active:
-			
-			if(StoryVariables.isPrologueDone == true):
+			if(StoryVariables.isInitiateMainQuestDone == true):
 				get_tree().paused = true
-				var dialog = Dialogic.start("initiateMainQuest")
+				var dialog = Dialogic.start("searchForStrings")
 				dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 				dialog.connect('timeline_end', self, 'unpause')
 				add_child(dialog)
-				StoryVariables.isInitiateMainQuestDone = true
-			elif(StoryVariables.isReportBackToMusicGeek == true):
+				StoryVariables.isSearchForStringsDone = true
+			elif(StoryVariables.isDeliverLetterToLover == true):
 				get_tree().paused = true
-				var dialog = Dialogic.start("deliverStringsToBard")
+				var dialog = Dialogic.start("reportBackToMusicGeek")
 				dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 				dialog.connect('timeline_end', self, 'unpause')
 				add_child(dialog)
-				StoryVariables.isDeliverStringsToBard = true
+				StoryVariables.isReportBackToMusicGeek = true
 
 func unpause(timeline_name):
 	get_tree().paused = false
 	active = false
 
-func _on_Bard_body_entered(body):
+func _on_MusicGeek_body_entered(body):
 	if body.name == 'player':
 		active = true
 
-func _on_Bard_body_exited(body):
+func _on_MusicGeek_body_exited(body):
 	if body.name == 'player':
 		active = false
--- Scenes/thrishaStuff/story_variables.gd
+++ Scenes/thrishaStuff/story_variables.gd
@@ -12,11 +12,19 @@ var isPrologueDone = false
 
 #CHAPTER ONE
 var isChapterOneStartDone = false
+
+#Getting Strings For Bard
 var isInitiateMainQuestDone = false
 var isSearchForStringsDone = false
 var isDeliverLetterToLover = false
 var isReportBackToMusicGeek = false
 var isDeliverStringsToBard = false
+
+#Trading Vendors
+var isCollectOtherworldlyItems = false;
+var isPleb_AItemCollected = false;
+var isPleb_BItemCollected = false;
+var isPleb_CItemCollected = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,6 +34,13 @@ func prologue_checkAllInteractions():
 	if (isMapInteracted == true && isAlisBedInteracted == true && isOtherBedInteracted == true && isMirrorInteracted == true):
 #		var dialog = Dialogic.start("movingInPart2")
 		var dialog = Dialogic.start("testing")
+		add_child(dialog)
+		yield(dialog, 'timeline_end')
+		isPrologueDone = true
+
+func chapter1_checkIfPlebItemsCollected():
+	if (isPleb_AItemCollected == true && isPleb_BItemCollected == true && isPleb_CItemCollected == true):
+		var dialog = Dialogic.start("collectOtherworldlyItems")
 		add_child(dialog)
 		yield(dialog, 'timeline_end')
 		isPrologueDone = true
