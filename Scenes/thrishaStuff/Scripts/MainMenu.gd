41fceb51fb8e23e05b722c0a933a08a7a5ca8820
Thrisha Kopula <thrishak1@gmail.com>
2023-05-09 12:31:20 -0700

    worked on dialogic themes

--- Scenes/thrishaStuff/Scripts/MainMenu.gd
+++ Scenes/thrishaStuff/Scripts/MainMenu.gd
@@ -11,10 +11,11 @@ func _ready():
 #	pass
 
 func _on_StartButton_pressed():
-	var dialog = Dialogic.start("movingIn")
+#	var dialog = Dialogic.start("movingIn")
+	var dialog = Dialogic.start("testing")
 	add_child(dialog)
 	yield(dialog, 'timeline_end')
-	get_tree().change_scene("res://Scenes/thrishaStuff/Locations/Overworld/Dorm.tscn")
+#	get_tree().change_scene("res://Scenes/thrishaStuff/Locations/Overworld/Dorm.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
