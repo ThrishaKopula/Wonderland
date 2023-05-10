4fd11782e66501e3642600819e053c7c70078f34
Junle Yuan <leo905991717@gmail.com>
2023-05-10 01:55:43 -0700

    trash

---
+++ Scenes/leoStuff/mini_game4/trash.gd
@@ -0,0 +1,40 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var hold = false;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	
+	if hold:
+		global_transform.origin = get_global_mouse_position();
+
+		
+	pass
+
+
+func _on_RigidBody2D_input_event(viewport, event, shape_idx):
+	
+	if Input.is_action_just_pressed("click"):
+		print("hold");
+		
+		mode = RigidBody2D.MODE_STATIC;
+		
+		hold = true;
+		
+	elif Input.is_action_just_released("click"):
+		print("not hold")
+		hold = false;
+		mode = RigidBody2D.MODE_RIGID;
+
+	
+	pass # Replace with function body.
