68e1adc2832674c5c21dfd4f4ed0abce870b7452
overlordmimi <minmini@gmail.com>
2023-05-17 19:53:12 -0700

    Revert "Merge branch 'main' into mimiBranch"
    This reverts commit ea0005628b99d0974afdc62fb66504e5f6511678, reversing
    changes made to df5e6cd9c1b31f8b420f80c0989f4e53ab342f65.

--- Scenes/thrishaStuff/Scripts/Dorm.gd
+++ Scenes/thrishaStuff/Scripts/Dorm.gd
@@ -10,6 +10,7 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ($DormAudio.playing == false):
