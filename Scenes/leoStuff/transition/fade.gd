4fbd7745ea544c21848986510d2919964a0baa14
Junle Yuan <leo905991717@gmail.com>
2023-05-27 21:10:12 -0700

    fade added

---
+++ Scenes/leoStuff/transition/fade.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func change_scene(scene: String):
+	
+	$AnimationPlayer.play("fade");
+	yield($AnimationPlayer, "animation_finished");
+	get_tree().change_scene(scene);
+	$AnimationPlayer.play_backwards("fade");
+	pass
