ee62387a7b1f7c87c5ec6357b516ffa4b883b1a8
Thrisha Kopula <thrishak1@gmail.com>
2023-05-27 22:53:19 -0700

    connected prologue to continue

---
+++ Scenes/thrishaStuff/Scripts/ContinueToChapter1.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Continue_pressed():
+	var dialog = Dialogic.start("chapter1Start")
+	add_child(dialog)
+	yield(dialog, 'timeline_end')
+	StoryVariables.isChapterOneStartDone = true
+	get_tree().change_scene("res://Scenes/thrishaStuff/Locations/Overworld/Classroom.tscn")
+
+func _on_Continue_button_down():
+	$ButtonClick.play()
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://Scenes/thrishaStuff/MainMenu.tscn")
+
+func _on_Main_Menu_button_down():
+	$ButtonClick.play()
