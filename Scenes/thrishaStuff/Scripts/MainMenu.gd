b3d40992f66f84094057df2b45d2510a8f26a7dd
Thrisha Kopula <thrishak1@gmail.com>
2023-05-29 13:20:25 -0700

    audio stuff

--- Scenes/thrishaStuff/Scripts/MainMenu.gd
+++ Scenes/thrishaStuff/Scripts/MainMenu.gd
@@ -1,10 +1,19 @@
 extends Control
 
+var canAudioPlay = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func checkAudio():
+	if(canAudioPlay == true):
+		$MenuAudio.play()
+	else:
+		$MenuAudio.stop()
+		$MenuAudio.playing == false
+		
 func _on_StartButton_pressed():
+	canAudioPlay = false
 	var dialog = Dialogic.start("testing")
 	add_child(dialog)
 	yield(dialog, 'timeline_end')
@@ -29,8 +38,9 @@ func _on_Credits_button_down():
 	$ButtonClick.play()
 	
 func _on_Prologue_pressed():
+	canAudioPlay = false
+	checkAudio()
 	var dialog = Dialogic.start("movingIn")
-#	var dialog = Dialogic.start("afterMiniGame")
 	add_child(dialog)
 	yield(dialog, 'timeline_end')
 	get_tree().change_scene("res://Scenes/thrishaStuff/Locations/Overworld/Dorm.tscn")
@@ -39,6 +49,8 @@ func _on_Prologue_button_down():
 	$ButtonClick.play()
 	
 func _on_Chapter_1_pressed():
+	canAudioPlay = false
+	checkAudio()
 	var dialog = Dialogic.start("chapter1Start")
 	add_child(dialog)
 	yield(dialog, 'timeline_end')
@@ -50,6 +62,8 @@ func _on_Chapter_1_button_down():
 	$ButtonClick.play()
 
 func _on_Chapter_2_pressed():
+	canAudioPlay = false
+	checkAudio()
 	var dialog = Dialogic.start("chapter2Start")
 	add_child(dialog)
 	yield(dialog, 'timeline_end')
