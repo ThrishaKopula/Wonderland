2a8425dc56c2c7151f39bc7841c94ce4ed32ddf2
Thrisha Kopula <thrishak1@gmail.com>
2023-05-29 15:29:01 -0700

    thief game

--- Scenes/thrishaStuff/Scripts/Tavern.gd
+++ Scenes/thrishaStuff/Scripts/Tavern.gd
@@ -1,26 +1,22 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ($TavernAudio.playing == false):
 		$TavernAudio.play()
-	pass
-
+	if(StoryVariables.isInThiefGame == true and StoryVariables.catch_tavern == false):
+		$Thief.show()
+	else:
+		$Thief.hide()
 
 func _on_Thief_body_entered(body):
 	StoryVariables.isInTavern = true
 
 func _on_Thief_body_exited(body):
 	StoryVariables.isInTavern = false
-	$Thief.queue_free()
+	StoryVariables.catch_tavern = true
+	$Thief.hide()
