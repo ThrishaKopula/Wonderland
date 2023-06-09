f92b685e749e9d72fdec004b51f0c9956be0bb00
Thrisha Kopula <thrishak1@gmail.com>
2023-06-08 22:03:41 -0700

    game over scene

--- Scenes/thrishaStuff/Scripts/GameOver.gd
+++ Scenes/thrishaStuff/Scripts/GameOver.gd
@@ -1,5 +1,11 @@
 extends Control
 
+
+func _process(delta):
+	if ($continueAudio.playing == false):
+		$continueAudio.play()
+	pass
+
 func _on_MainMenu_pressed():
 	Fade.change_scene("res://Scenes/thrishaStuff/MainMenu.tscn")
 	yield(get_tree().create_timer(2), "timeout")
